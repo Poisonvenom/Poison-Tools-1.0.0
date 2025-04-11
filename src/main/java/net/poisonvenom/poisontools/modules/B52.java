cb09bdd253e74147ecde87887be9d6d0cdf0dc6f
Poisonvenom <poisonvenomwastaken@gmail.com>
2025-04-10 22:48:44 -0400

    Actually got B52 to work

--- src/main/java/net/poisonvenom/poisontools/modules/B52.java
+++ src/main/java/net/poisonvenom/poisontools/modules/B52.java
@@ -2,12 +2,12 @@ package net.poisonvenom.poisontools.modules;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Blocks;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Items;
-import net.minecraft.text.Text;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.poisonvenom.poisontools.PoisonTools;
 
@@ -20,11 +20,14 @@ public class B52 extends Module {
     @Override
     public void onActivate() {
         tick = 0;
+        mc.options.useKey.setPressed(true);
+        info("Bombs away!");
     }
 
     @Override
     public void onDeactivate() {
         tick = 0;
+        mc.options.useKey.setPressed(false);
     }
 
     @EventHandler
@@ -34,7 +37,7 @@ public class B52 extends Module {
 
     private void mainAlgorithm() {
         if (mc.world == null || mc.player == null || mc.crosshairTarget == null) return;
-        ChatUtils.sendMsg(Text.of("" + tick));
+        //ChatUtils.sendMsg(Text.of("" + tick));
         Inventory inventory = mc.player.getInventory();
         int tntSlot = -1, flintSlot = -1;
         for (int i = 0; i < 9; i++) {
@@ -45,25 +48,44 @@ public class B52 extends Module {
             }
         }
 
-        if (tick == 0) {
-            mc.player.getInventory().setSelectedSlot(tntSlot);
-            mc.options.useKey.setPressed(true);
-            ChatUtils.sendMsg(Text.of("tnt click"));
-            tick++;
-        } else if (tick == 2) {
-            mc.options.useKey.setPressed(false);
-            ChatUtils.sendMsg(Text.of("tnt unclick"));
-            tick++;
-        } else if (tick == 4) {
-            if (mc.world.getBlockState(BlockPos.ofFloored(mc.crosshairTarget.getPos())).getBlock() == Blocks.TNT) {
+        if (mc.crosshairTarget.getType() == HitResult.Type.BLOCK) {
+            BlockHitResult bhr = (BlockHitResult) mc.crosshairTarget;
+            BlockPos pos = bhr.getBlockPos();
+
+            // Get the block at that position
+            if (mc.world.getBlockState(pos).getBlock() == Blocks.TNT) {
                 mc.player.getInventory().setSelectedSlot(flintSlot);
-                mc.options.useKey.setPressed(true);
+            } else {
+                mc.player.getInventory().setSelectedSlot(tntSlot);
             }
-            tick++;
-        } else if (tick == 6) {
-            mc.options.useKey.setPressed(false);
-            tick = -1;
+        } else {
+            mc.player.getInventory().setSelectedSlot(tntSlot);
         }
-        tick++;
+        //ChatUtils.sendMsg(Text.of(mc.crosshairTarget.getPos().toString()));
+//        if (mc.world.getBlockState(BlockPos.ofFloored(mc.crosshairTarget.getPos())).getBlock() == Blocks.TNT) {
+//            mc.player.getInventory().setSelectedSlot(flintSlot);
+//        } else {
+//            mc.player.getInventory().setSelectedSlot(tntSlot);
+//        }
+//        if (tick == 0) {
+//            mc.player.getInventory().setSelectedSlot(tntSlot);
+//            mc.options.useKey.setPressed(true);
+//            ChatUtils.sendMsg(Text.of("tnt click"));
+//            tick++;
+//        } else if (tick == 2) {
+//            if () {
+//                mc.options.useKey.setPressed(false);
+//            } else {
+//                mc.player.getInventory().setSelectedSlot(flintSlot);
+//            }
+//            tick++;
+//        } else if (tick == 4) {
+//
+//            tick++;
+//        } else if (tick == 6) {
+//            mc.options.useKey.setPressed(false);
+//            tick = -1;
+//        }
+//        tick++;
     }
 }
